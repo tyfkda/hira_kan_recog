853d0687aeb8934e70b7102dd5e904f9beaf9400
tyfkda <tyfkda@gmail.com>
2022-05-11 10:38:54 +0900

    TFJSで文字認識させる

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,197 @@
-// import {Util, generatorTest} from './util/util'
+import * as tf from '@tensorflow/tfjs'
 
-function getPosition(element: HTMLElement, clientX: number, clientY: number): {x: number, y: number} {
-  const rect = element.getBoundingClientRect()
-  return {
-    x: clientX - rect.left,
-    y: clientY - rect.top,
-  }
+const BG_COLOR = '#F8F8F8'
+
+const CLASS = 'あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろわをん愛悪圧安暗案以位依囲委意易異移胃遺医育一壱印員因引飲院右雨運雲営栄永泳英衛液益駅円園延演遠塩央往応横王黄億屋恩温音下化仮何価加可夏家科果歌河火花荷課貨過我画芽賀会解回快改械海界絵開階貝外害各拡格確覚角革学楽額活株寒刊勧完官幹感慣歓漢管観間関館岸眼岩顔願喜器基寄希旗期機帰気汽季紀規記貴起技疑義議客逆久休宮急救求球究級給旧牛去居挙許漁魚京供競共協境強教橋興鏡業局曲極玉勤均禁近金銀九句区苦具空君訓群軍郡係兄型形敬景系経計軽芸欠決潔結血月件健兼券建憲検権犬研絹県見険験元原厳減現言限個古固己庫戸故湖五午後語誤護交候光公功効厚口向后孝工幸広康校構港皇耕考航行講鉱高号合告国穀黒今根混左差査再最妻才採済災祭細菜際在材罪財坂作昨策刷察殺雑三参山散産算蚕賛酸残仕使司史四士始姉子市師志思指支止死氏私糸紙至視詞詩試資歯事似児字寺持時次治示耳自辞式識七失室質実舎写社者謝車借釈弱主取守手種酒首受授需収周宗就州修拾秋終習衆週集住十従重宿祝出術述春準純順処初所暑書諸助女序除勝商唱小少承招昭消焼照省称章証象賞上乗場常情条状植織職色食信心新森深申真神臣親身進人仁図推水数世是制勢性成政整星晴正清生精聖声製西誠青静税席石積績責赤切接折設節説雪絶舌先千宣専川戦浅線船選銭前善然全祖素組創倉想早争相総草走送像増蔵造側則息測足速俗属族続卒存孫尊損村他多太打体対帯待態貸退隊代台大第題達谷単炭短団断男談知地池置築竹茶着中忠昼柱注虫著貯丁帳張朝町腸調長鳥直賃追通低停定底庭弟提程敵的適鉄典天展店転点伝田電徒登都努度土党冬刀島投東湯燈当等答統討頭働動同堂導童道銅得徳特毒独読届内南難二弐肉日入任認熱年念燃納能農波派破馬拝敗配倍買売博白麦畑八発判半反板版犯飯番否悲比皮肥費非飛備美鼻必筆百俵標氷票表評病秒品貧不付夫婦富布府父負武部風復幅服福複仏物分奮粉文聞兵平陛米別変編辺返便勉弁保歩補墓母包報放方法豊暴望貿防北牧本妹毎末万満味未脈民務無名命明盟迷鳴綿面毛木目問門夜野役約薬訳油輸勇友有由遊夕予余預容曜様洋用葉要陽養欲浴来落利理里陸律率立略流留旅両料良量領力緑林臨輪類令例冷礼歴列練連路労老六録論和話'
+
+class Vector2 {
+  constructor(public x: number, public y: number) {}
 }
 
-class App {
-  private ctx: CanvasRenderingContext2D
-  private cursorPos = {x: -1, y: -1}
-  private drawing = false
+function setMouseDragListener(mouseMove: any, mouseUp?: (e)=>void, useCapture?: any) {
+  let mouseLeave: ((e)=>boolean) | undefined
 
-  constructor(private canvas: HTMLCanvasElement) {
-    console.log(this.canvas)
-
-    this.ctx = this.canvas.getContext('2d')!
-
-    this.initialize()
-    this.canvas.addEventListener('mousedown', (event) => {
-      if (event.button !== 0)
-        return false
-      this.cursorPos = getPosition(this.canvas, event.clientX, event.clientY);
-      this.drawing = true
-      return true
-    })
-    document.addEventListener('mousemove', (event) => {
-      if (!this.drawing)
-        return
-      const curr = getPosition(this.canvas, event.clientX, event.clientY);
-      this.ctx.lineWidth = 16
-      this.ctx.lineCap = 'round'
-      this.ctx.beginPath()
-      this.ctx.moveTo(this.cursorPos.x, this.cursorPos.y)
-      this.ctx.lineTo(curr.x, curr.y)
-      this.ctx.stroke()
-      this.ctx.closePath()
-      this.cursorPos = curr
-    })
-    document.addEventListener('mouseup', (_event) => {
-      this.drawing = false
-    })
+  if (typeof mouseMove === 'object') {
+    const option = mouseMove
+    mouseMove = option.move
+    mouseUp = option.up
+    mouseLeave = option.leave
+    useCapture = option.useCapture
   }
 
-  initialize(): void {
-    this.ctx.fillStyle = '#FFFFFF'
-    this.ctx.fillRect(0, 0, 449, 449)
-    this.ctx.lineWidth = 1
-    this.ctx.strokeRect(0, 0, 449, 449)
-    this.ctx.lineWidth = 0.05
-    for (let i = 0; i < 27; i++) {
-      this.ctx.beginPath()
-      this.ctx.moveTo((i + 1) * 16,   0)
-      this.ctx.lineTo((i + 1) * 16, 449)
-      this.ctx.closePath()
-      this.ctx.stroke()
+  const mouseUpDelegate = ($event) => {
+    if (mouseUp)
+      mouseUp($event)
+    unlisten()
+  }
 
-      this.ctx.beginPath()
-      this.ctx.moveTo(  0, (i + 1) * 16)
-      this.ctx.lineTo(449, (i + 1) * 16)
-      this.ctx.closePath()
-      this.ctx.stroke()
+  const mouseLeaveDelegate = (mouseLeave == null ? null : ($event) => {
+    if (mouseLeave!($event))
+      unlisten()
+  })
+
+  const unlisten = () => {
+    document.removeEventListener('mousemove', mouseMove, useCapture)
+    document.removeEventListener('touchmove', mouseMove, useCapture)
+    document.removeEventListener('mouseup', mouseUpDelegate, useCapture)
+    document.removeEventListener('touchend', mouseUpDelegate, useCapture)
+    if (mouseLeaveDelegate) {
+      document.removeEventListener('mouseleave', mouseLeaveDelegate, useCapture)
+      document.removeEventListener('touchcancel', mouseLeaveDelegate, useCapture)
     }
   }
 
-  run(): void {
+  document.addEventListener('mousemove', mouseMove, useCapture)
+  document.addEventListener('touchmove', mouseMove, useCapture)
+  document.addEventListener('mouseup', mouseUpDelegate, useCapture)
+  document.addEventListener('touchend', mouseUpDelegate, useCapture)
+  if (mouseLeaveDelegate) {
+    document.addEventListener('mouseleave', mouseLeaveDelegate, useCapture)
+    document.addEventListener('touchcancel', mouseLeaveDelegate, useCapture)
   }
 }
 
-window.addEventListener('load', () => {
-  const canvas = document.getElementById('mycanvas') as HTMLCanvasElement
-  const app = new App(canvas)
-  app.run()
+function getMousePosition(event, canvas: HTMLCanvasElement): Vector2 {
+  let clientX: number, clientY: number
+  if (event.changedTouches) {
+    const touch = event.changedTouches[0]
+    clientX = touch.clientX
+    clientY = touch.clientY
+  } else {
+    clientX = event.clientX
+    clientY = event.clientY
+  }
+  const rect = canvas.getBoundingClientRect()
+  return new Vector2(
+    (clientX - rect.left) * canvas.width / rect.width,
+    (clientY - rect.top) * canvas.height / rect.height)
+}
+
+class App {
+  private canvas: HTMLCanvasElement
+  private context: CanvasRenderingContext2D
+  private strokes: Array<Array<Vector2>> = []
+
+  private miniCanvas: HTMLCanvasElement
+  private miniCanvasContext: CanvasRenderingContext2D
+
+  constructor(private model: tf.GraphModel) {
+    const inputShape = model.inputs[0].shape!
+    this.miniCanvas = document.getElementById('mini') as HTMLCanvasElement
+    this.miniCanvas.width = inputShape[inputShape.length - 2]
+    this.miniCanvas.height = inputShape[inputShape.length - 3]
+    this.miniCanvasContext = this.miniCanvas.getContext('2d')!
+
+    const canvas = document.getElementById('mycanvas') as HTMLCanvasElement
+    canvas.style.width = `${window.innerWidth}px`
+    canvas.style.height = `${window.innerHeight}px`
+    canvas.width = window.innerWidth * window.devicePixelRatio
+    canvas.height = window.innerHeight * window.devicePixelRatio
+    this.canvas = canvas
+
+    this.context = canvas.getContext('2d')!
+    this.context.lineWidth = window.devicePixelRatio
+
+    const mouseDown = (event) => this.mouseDown(event)
+    const passive = {passive: false}
+    canvas.addEventListener('mousedown', mouseDown, passive)
+    canvas.addEventListener('touchstart', mouseDown, passive)
+
+    document.getElementById('clear')!.addEventListener('click', (_event) => {
+      this.clear()
+    })
+
+    this.clear()
+  }
+
+  clear(): void {
+    this.strokes.length = 0
+    this.drawMini()
+
+    this.context.fillStyle = BG_COLOR
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  mouseDown(event): void {
+    if (event.type === 'mousedown' && event.button !== 0)
+      return
+    event.preventDefault()
+
+    const canvas = this.canvas
+    const points: Array<Vector2> = []
+    let lastPos = getMousePosition(event, canvas)
+    points.push(lastPos)
+
+    const upFunc = (_event) => {
+      if (points.length >= 2)
+        this.strokes.push(points)
+      this.drawMini()
+      this.recognize()
+    }
+
+    setMouseDragListener({
+      move: (event) => {
+        const pos = getMousePosition(event, canvas)
+        const context = this.context
+
+        context.strokeStyle = '#000'
+        context.lineWidth = 16
+        context.lineCap = 'round'
+        context.beginPath()
+        context.moveTo(lastPos.x, lastPos.y)
+        context.lineTo(pos.x, pos.y)
+        context.stroke()
+        lastPos = pos
+
+        points.push(pos)
+      },
+      up: upFunc,
+      leave: upFunc,
+    })
+  }
+
+  drawMini(): void {
+    const context = this.miniCanvasContext
+    const w = this.miniCanvas.width
+    const h = this.miniCanvas.height
+    const S = 2
+    context.fillStyle = '#000'
+    context.fillRect(0, 0, w, h)
+    context.lineWidth = S
+
+    const invW = w / window.innerWidth
+    const invH = h / window.innerHeight
+    for (const points of this.strokes) {
+      context.strokeStyle = '#fff'
+      context.beginPath()
+      points.forEach((pos, i) => {
+        if (i === 0)
+          context.moveTo(pos.x * invW, pos.y * invH)
+        else
+          context.lineTo(pos.x * invW, pos.y * invH)
+      })
+      context.stroke()
+    }
+  }
+
+  recognize(): void {
+    tf.tidy(() => {
+      const x = tf.browser.fromPixels(this.miniCanvas, 1).expandDims().toFloat()
+      const y = this.model.predict(x, {batchSize: 1}) as tf.Tensor
+      const yArray = y.arraySync()
+      const probabilities = yArray[0]
+
+      const order = [...Array(probabilities.length)].map((_, i) => i).filter(i => probabilities[i] >= 0.005)
+      order.sort((a, b) => probabilities[b] - probabilities[a])
+      const candidates = order.slice(0, 5)
+      console.log(candidates.map(i => `${CLASS[i]}: ${probabilities[i] * 100}`).join('\n'))
+    })
+  }
+}
+
+window.addEventListener('load', async () => {
+  const path = 'assets/tfjsmodel/model.json'
+  const model = await tf.loadGraphModel(path)
+
+  new App(model)
 })
